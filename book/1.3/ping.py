84645af6219cac02d4ac3a17f10ca9e8fee64be4
office-deskjet <office_deskjet@protonmail.com>
2019-03-22 19:23:00 -0400

    adding ping sweep in python. finished ping in bash

---
+++ book/1.3/ping.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2
+# only works in python2 without modifying the library
+import pyping
+
+base_ip = "10.11.1."
+
+for i in range(0,256):
+    ip = base_ip + str(i)
+    resp = pyping.ping(ip)
+    if resp.ret_code == 0:
+        print ip
+
