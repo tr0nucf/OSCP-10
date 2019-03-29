c9852a4347004bd6afe2276812e707a3b15a3590
office-deskjet <office_deskjet@protonmail.com>
2019-03-29 17:59:22 -0400

    code cleaning, adding new examples

--- book/1.3/ping.py
+++ book/1.3/ping.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2
 # only works in python2 without modifying the library
 import pyping
+import sys
 
 base_ip = "10.11.1."
 
@@ -9,4 +10,5 @@ for i in range(0,256):
     resp = pyping.ping(ip)
     if resp.ret_code == 0:
         print ip
+        sys.stdout.flush()
 
--- book/1.3/ping.sh
+++ book/1.3/ping.sh
@@ -2,5 +2,5 @@
 
 for i in 10.11.1.{0..255}
 do
-        ping $i -c 4 | grep " 0% packet loss" -B1 | grep "ping statistics" | cut -d " " -f 2 >> ip_list.txt
+        ping $i -c 1 | grep " 0% packet loss" -B1 | grep "ping statistics" | cut -d " " -f 2 >> ip_list.txt
 done
