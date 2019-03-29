f8372eadda31dc1645bb992de47dc99d450b94b3
office-deskjet <office_deskjet@protonmail.com>
2019-03-29 18:08:43 -0400

    fxied dns script

--- book/4.1.7/dns_zone_tx.sh
+++ book/4.1.7/dns_zone_tx.sh
@@ -2,12 +2,11 @@
 
 if [ -z "$1" ]; then
         echo "[*] Simple zone transfer script"
-        echo "[*] Usage    : $0 <domain name> "
+        echo "[*] Usage:    $0 <domain name> "
         exit 0
 fi
 
-
-for server in $(host -t ns $$1 | cut -d " " -f 4); do
+for server in $(host -t ns $1 | cut -d " " -f 4); do
         host -l $1 $server | grep "has address"
 done
 
